eb5f867650a090d88c59d69eb8d9e487ce73e6c0
Emre Çakmak <emreckmk98@hotmail.com>
2021-02-24 21:39:07 +0300

    guess app

---
+++ constants/default-styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+
+export default StyleSheet.create({
+    bodyText:{
+        fontFamily:'myOpen-sans',        
+    },
+    title:{
+        fontFamily:'myOpen-sans-bold',
+        fontSize:18,
+    },
+});
